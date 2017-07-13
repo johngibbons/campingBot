04fcb92037db310c2e0535a2348392d9c64f2b9a
John Gibbons <johngibbons10@gmail.com>
2017-07-13 01:05:15 -0700

    set up .env file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const url = 'mongodb://localhost:27017/campingReserver'
+const url = process.env.MONGODB_URI
 const bodyParser = require('body-parser')
 const timerTest = require('./timerTest')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const seedCampgrounds = require('./data/seedCampgrounds')
-const generateDates = require('./datesGenerator')
 
 app.set('port', process.env.PORT || 8080)
 app.use(bodyParser.urlencoded({ extended: true }))
